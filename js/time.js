3598533b43fe7cab35b46411e3bf269624db9ba9
RoseMizuki <nikonf4.amy@gmail.com>
2016-11-04 11:54:37 -0700

    Update time.js

--- js/time.js
+++ js/time.js
@@ -1,6 +1,7 @@
 <!-
 function startTime() {
     var today = new Date();
+    
     var h = today.getHours();
     var m = today.getMinutes();
     var s = today.getSeconds();
@@ -8,10 +9,29 @@ function startTime() {
     s = checkTime(s);
     document.getElementById('txt').innerHTML =
     h + ":" + m + ":" + s;
-    var t = setTimeout(startTime, 500);
+    var t = setInterval(startTime, 500);
 }
 function checkTime(i) {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
     return i;
 }
+function calcTime(city, offset) {
+
+    // create Date object for current location
+    d = new Date();
+    
+    // convert to msec
+    // add local time zone offset 
+    // get UTC time in msec
+    utc = d.getTime() + (d.getTimezoneOffset() * 60000);
+    
+    // create new Date object for different city
+    // using supplied offset
+    nd = new Date(utc + (3600000*offset));
+    
+    // return time as a string
+    return "The local time in " + city + " is " + nd.toLocaleString();
+
+}
+
 ->
